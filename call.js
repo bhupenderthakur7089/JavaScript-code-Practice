4c717952a8530005e4198007fc9c1b0a810d82e2
bhupenderthakur7089 <117012030+bhupenderthakur7089@users.noreply.github.com>
2023-05-15 00:48:58 +0530

    first commit

---
+++ call.js
@@ -0,0 +1,6 @@
+function display(){
+    console.log(document.getElementById('name').value);
+    console.log(document.getElementById('mail').value);
+    console.log(document.getElementById('phone').value);
+    console.log(document.getElementById('date').value);
+};
